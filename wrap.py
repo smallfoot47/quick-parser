501de9c640a35a9655613ff2626f88c946ae6f34
smallfoot <smallfoot@osiris.cyber.nyu.edu>
2024-03-11 02:44:09 -0400

    Customized the formatter for future development

--- wrap.py
+++ wrap.py
@@ -1,16 +1,39 @@
 import re
 
 class Wrapper:
-    def __init__(self, header:str='', body_fmt:str='', footer:str='', axe:str=' ', glue=',') -> None:
+    def __init__(self, header:str='', body_fmt:str='', footer:str='', axe:str=' ', glue=',', arg_prep:int=10) -> None:
         self.__h = header
         self.__b = body_fmt
+        self.__arg_count(10)
         self.__f = footer
         self.__a = axe
         self.__g = glue
     
+    def __arg_count(self, guess:int) -> None:
+        if "{" + str(guess) + "}" in self.__b:
+            guess += 1
+            while "{" + str(guess) + "}" in self.__b: guess += 1
+            return guess
+        else:
+            guess -= 1
+            while "{" + str(guess) + "}" not in self.__b: guess -= 1
+        
+        self.__argc = guess
+
     def __fix_string(self, ajar:str) -> str:
         apickle = re.sub(r'(?<!\\)\\n', r'\n', ajar)
         return apickle
+    
+    def __format(self, form, tokens) -> str:
+        argc = min(self.__argc, len(tokens))
+
+        for idx, token in enumerate(tokens):
+            form = form.replace("{"+ str(idx) + "}", token)
+        
+        for idx in range(argc, self.__argc):
+            form = form.replace("{" + str(idx) + "}", '')
+        
+        return form
     
     def Set(self, header:str=None, body_fmt:str=None, footer:str=None, axe:str=None, glue:str=None) -> None:
         if header != None:
@@ -34,7 +57,7 @@ class Wrapper:
                 
                 for line in data_file.read().splitlines():
                     tokens = line.split(self.__a)
-                    body.append(self.__b.format(*tokens))
+                    body.append(self.__format(self.__b, tokens))
                 
                 wrapping += self.__fix_string(self.__g.join(body))
                 
