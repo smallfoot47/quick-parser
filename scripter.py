9b4f017a7de17997655bd7c9901e74128248bf81
smallfoot <smallfoot@osiris.cyber.nyu.edu>
2024-03-11 21:41:48 -0400

    Updated scripter.py: Added Instruction class

--- scripter.py
+++ scripter.py
@@ -1,25 +1,44 @@
 import re
 
+class Instruction:
+    def __init__(self, preproc=[], presub=[], postsub=[], postproc=[]) -> None:
+        self.__cat = {
+            '~' : preproc,
+            '-' : presub,
+            '+' : postsub,
+            '*' : postproc
+        }
+    
+    def Get(self, key:str) -> tuple:
+        return self.__cat[key]
+    
+    def Set(self, key:str, value:tuple) -> None:
+        self.__cat[key] = value
+    
+
 class Scripter:
     def __init__(self, scriptFile:str) -> None:
         self.__parse(scriptFile)
     
     def __parse(self, scriptFile) -> None:
         with open(scriptFile) as script:
-            snip = script.readline()
+            self.__instr = Instruction()
 
-            if len(snip) > 0:
+            for snippet in script.readlines():
+                instruction = None
 
-                if snip[0] == '~':
-                    # pre-proc
-                    pass
-                elif snip[0] == '-':
-                    # pre-sub
-                    pass
-                elif snip[0] == '+':
-                    # post-sub
-                    pass
-                elif snip[0] == '*':
-                    # post-proc
-                    pass
-            
+                if len(snippet) > 0:
+                    if snippet[0] == '~':
+                        # pre-proc
+                        pass
+                    elif snippet[0] == '-':
+                        # pre-sub
+                        pass
+                    elif snippet[0] == '+':
+                        # post-sub
+                        pass
+                    elif snippet[0] == '*':
+                        # post-proc
+                        pass
+                
+                    self.__instr[snippet[0]].append(instruction)
